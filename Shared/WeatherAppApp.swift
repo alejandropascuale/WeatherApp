075c06bfea53208d432fae03a2ab78137ee9c98b
alejandropascuale <apascuale@outlook.com>
2022-02-17 16:23:29 -0300

    Initial Commit

---
+++ Shared/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  Shared
+//
+//  Created by Alejandro Pascuale on 17/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
