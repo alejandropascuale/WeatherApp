2c3a21a2a2c3cf2faae8975aa35f008871f3abd7
alejandropascuale <apascuale@outlook.com>
2022-02-23 10:39:56 -0300

    create loading view and show coordinates when loading finish

---
+++ Views/LoadingView.swift
@@ -0,0 +1,22 @@
+//
+//  LoadingView.swift
+//  WeatherApp
+//
+//  Created by Alejandro Pascuale on 23/02/2022.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        ProgressView()
+            .progressViewStyle(CircularProgressViewStyle(tint: .white))
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
