b95e7528354098ee5dc0448ebdb39f3fcfd9fbff
alejandropascuale <apascuale@outlook.com>
2022-02-23 15:07:07 -0300

    create extension file

---
+++ Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Extensions.swift
+//  WeatherApp (iOS)
+//
+//  Created by Alejandro Pascuale on 23/02/2022.
+//
+
+import Foundation
+
+extension Double {
+    func roundDouble() -> String {
+        return String(format: "%.0f", self)
+    }
+}
